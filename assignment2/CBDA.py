ffdf4d04d529831a5b5c6beca52ad367b566ea19
DJongstra <danielle.jongstra@student.uantwerpen.be>
2020-11-25 23:31:59 +0100

    CBDA en CBDB with error

--- assignment2/CBDA.py
+++ assignment2/CBDA.py
@@ -35,7 +35,7 @@ def get_block(block, path=""):
 
 class CBDA(CBD):
 	def __init__(self, block_name, deltaT=0.1):
-		CBD.__init__(self, block_name, input_ports=[], output_ports=['OUT1'])
+		CBD.__init__(self, block_name, input_ports=[], output_ports=['OUT1', 'error'])
 		
 		# Create the blocks
 		self.addBlock(ConstantBlock(block_name='x0', value=(0)))
@@ -44,6 +44,11 @@ class CBDA(CBD):
 		self.addBlock(ConstantBlock(block_name='deltaT', value=(deltaT)))
 		self.addBlock(IntegratorBlock(block_name='7IsC0muY9sn0-m5gdAO--8'))
 		self.addBlock(IntegratorBlock(block_name='7IsC0muY9sn0-m5gdAO--14'))
+		self.addBlock(SinFunc(block_name='qr4DKPYwkg5aO8gcdtgf-35'))
+		self.addBlock(AdderBlock(block_name='qr4DKPYwkg5aO8gcdtgf-39'))
+		self.addBlock(ABSBlock(block_name='qr4DKPYwkg5aO8gcdtgf-45'))
+		self.addBlock(IntegratorBlock(block_name='qr4DKPYwkg5aO8gcdtgf-49'))
+		self.addBlock(ConstantBlock(block_name='qr4DKPYwkg5aO8gcdtgf-60', value=(0)))
 		
 		# Connect the blocks
 		self.addConnection('deltaT', '7IsC0muY9sn0-m5gdAO--8', input_port_name='delta_t')
@@ -54,6 +59,30 @@ class CBDA(CBD):
 		self.addConnection('initdxdt', '7IsC0muY9sn0-m5gdAO--8', input_port_name='IC')
 		self.addConnection('x0', '7IsC0muY9sn0-m5gdAO--14', input_port_name='IC')
 		self.addConnection('WgBMcyxID5A1E6gqlP3p-31', '7IsC0muY9sn0-m5gdAO--8')
+		self.addConnection('deltaT', 'qr4DKPYwkg5aO8gcdtgf-35', input_port_name='Din')
+		self.addConnection('qr4DKPYwkg5aO8gcdtgf-35', 'qr4DKPYwkg5aO8gcdtgf-39', output_port_name='sinOut')
+		self.addConnection('7IsC0muY9sn0-m5gdAO--14', 'qr4DKPYwkg5aO8gcdtgf-39')
+		self.addConnection('qr4DKPYwkg5aO8gcdtgf-39', 'qr4DKPYwkg5aO8gcdtgf-45')
+		self.addConnection('qr4DKPYwkg5aO8gcdtgf-45', 'qr4DKPYwkg5aO8gcdtgf-49')
+		self.addConnection('deltaT', 'qr4DKPYwkg5aO8gcdtgf-49', input_port_name='delta_t')
+		self.addConnection('qr4DKPYwkg5aO8gcdtgf-49', 'error')
+		self.addConnection('qr4DKPYwkg5aO8gcdtgf-60', 'qr4DKPYwkg5aO8gcdtgf-49', input_port_name='IC')
+
+
+class SinFunc(CBD):
+	def __init__(self, block_name):
+		CBD.__init__(self, block_name, input_ports=['Din'], output_ports=['sinOut'])
+		
+		# Create the blocks
+		self.addBlock(TimeBlock(block_name='time'))
+		self.addBlock(ProductBlock(block_name='prodSin'))
+		self.addBlock(GenericBlock(block_name='sin', block_operator=("sin")))
+		
+		# Connect the blocks
+		self.addConnection('time', 'prodSin')
+		self.addConnection('Din', 'prodSin')
+		self.addConnection('prodSin', 'sin')
+		self.addConnection('sin', 'sinOut')
 
 
 if __name__ == '__main__':
@@ -65,3 +94,4 @@ if __name__ == '__main__':
 
 	# process simulation results
 	plot_signal(cbd, ['OUT1'], 'Harmonic A')
+	plot_signal(cbd, ['error'], 'Error Harmonic A')
--- assignment2/CBDB.py
+++ assignment2/CBDB.py
@@ -34,7 +34,7 @@ def get_block(block, path=""):
 
 class CBDB(CBD):
 	def __init__(self, block_name, deltaT=0.1):
-		CBD.__init__(self, block_name, input_ports=[], output_ports=['OUT1'])
+		CBD.__init__(self, block_name, input_ports=[], output_ports=['OUT1', 'error'])
 		
 		# Create the blocks
 		self.addBlock(ConstantBlock(block_name='x0', value=(0)))
@@ -43,6 +43,11 @@ class CBDB(CBD):
 		self.addBlock(ConstantBlock(block_name='deltaT', value=(deltaT)))
 		self.addBlock(DerivatorBlock(block_name='CZ0sbhZwY290FykPwCE_-1'))
 		self.addBlock(DerivatorBlock(block_name='CZ0sbhZwY290FykPwCE_-6'))
+		self.addBlock(SinFunc(block_name='kQCSEosvlVg2ZfA-_8SE-22'))
+		self.addBlock(AdderBlock(block_name='kQCSEosvlVg2ZfA-_8SE-26'))
+		self.addBlock(ABSBlock(block_name='kQCSEosvlVg2ZfA-_8SE-32'))
+		self.addBlock(ConstantBlock(block_name='kQCSEosvlVg2ZfA-_8SE-43', value=(0)))
+		self.addBlock(IntegratorBlock(block_name='kQCSEosvlVg2ZfA-_8SE-36'))
 		
 		# Connect the blocks
 		self.addConnection('WgBMcyxID5A1E6gqlP3p-31', 'OUT1')
@@ -53,6 +58,30 @@ class CBDB(CBD):
 		self.addConnection('initdxdt', 'CZ0sbhZwY290FykPwCE_-1', input_port_name='IC')
 		self.addConnection('x0', 'CZ0sbhZwY290FykPwCE_-6', input_port_name='IC')
 		self.addConnection('WgBMcyxID5A1E6gqlP3p-31', 'CZ0sbhZwY290FykPwCE_-1')
+		self.addConnection('deltaT', 'kQCSEosvlVg2ZfA-_8SE-22', input_port_name='Din')
+		self.addConnection('CZ0sbhZwY290FykPwCE_-6', 'kQCSEosvlVg2ZfA-_8SE-26')
+		self.addConnection('kQCSEosvlVg2ZfA-_8SE-22', 'kQCSEosvlVg2ZfA-_8SE-26', output_port_name='sinOut')
+		self.addConnection('kQCSEosvlVg2ZfA-_8SE-26', 'kQCSEosvlVg2ZfA-_8SE-32')
+		self.addConnection('kQCSEosvlVg2ZfA-_8SE-43', 'kQCSEosvlVg2ZfA-_8SE-36', input_port_name='IC')
+		self.addConnection('deltaT', 'kQCSEosvlVg2ZfA-_8SE-36', input_port_name='delta_t')
+		self.addConnection('kQCSEosvlVg2ZfA-_8SE-36', 'error')
+		self.addConnection('kQCSEosvlVg2ZfA-_8SE-32', 'kQCSEosvlVg2ZfA-_8SE-36')
+
+
+class SinFunc(CBD):
+	def __init__(self, block_name):
+		CBD.__init__(self, block_name, input_ports=['Din'], output_ports=['sinOut'])
+		
+		# Create the blocks
+		self.addBlock(TimeBlock(block_name='time'))
+		self.addBlock(ProductBlock(block_name='prodSin'))
+		self.addBlock(GenericBlock(block_name='sin', block_operator=("sin")))
+		
+		# Connect the blocks
+		self.addConnection('time', 'prodSin')
+		self.addConnection('Din', 'prodSin')
+		self.addConnection('prodSin', 'sin')
+		self.addConnection('sin', 'sinOut')
 
 
 if __name__ == '__main__':
@@ -64,3 +93,5 @@ if __name__ == '__main__':
 
 	# process simulation results
 	plot_signal(cbd, ['OUT1'], 'Harmonic B')
+	plot_signal(cbd, ['error'], 'Error Harmonic B')
+
